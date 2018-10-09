3e7bb4b46af7a160a7368edb323d49aa5d1e36b9
= <thanhphonui321@gmail.com>
2018-10-09 13:01:04 +0700

    Add authentication

---
+++ BookShopWithAuthen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookShopWithAuthen.MvcApplication" Language="C#" %>
---
+++ BookShopWithAuthen/Models/Publisher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShopWithAuthen.Models
+{
+    public class Publisher
+    {
+        public Publisher()
+        {
+            Books = new HashSet<Book>();
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
